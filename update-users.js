1f340b9fe4d865eea157c81c43cebf3284d5b219
CylonVR <thefirstmellon@gmail.com>
2024-11-10 23:04:46 -0500

    Update update-users.js

--- update-users.js
+++ update-users.js
@@ -18,7 +18,7 @@ try {
         console.log("Type Subscription")
     }
     else {
-        amount = event_json.amount + ""
+        amount = event_json.amount + "€"
         console.log("Type Donation")
     }
     
