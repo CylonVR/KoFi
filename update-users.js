9a948ebb51134134e57a897fd694d32e30059849
CylonVR <thefirstmellon@gmail.com>
2024-11-20 21:03:06 -0500

    Update update-users.js

--- update-users.js
+++ update-users.js
@@ -14,26 +14,23 @@ try {
     let amount = event_json.amount;
 
     if(type == "Subscription") {
-        amount = event_json.amount + "€/mois"
         console.log("Type Subscription")
     }
     else {
-        amount = event_json.amount + "€"
         console.log("Type Donation")
     }
     
     if(!userArray.find(line => line[0] === event_json.name)) {
-        userArray.push([event_json.name, amount])
+        userArray.push([event_json.name])
         console.log("user added")
     }
     else {
         const index = userArray.findIndex(line => line[0] === event_json.name)
-        userArray[index][1] = amount
         console.log("updated existing user")
     }
     console.log(userArray)
 
-    const textRaw = userArray.map((line) => line.join(" : ")).join("\n")
+    const textRaw = userArray.map((line)
     fs.writeFileSync(FILE_PATH, textRaw, { encoding: "utf8" })
     console.log("file written")
 }
