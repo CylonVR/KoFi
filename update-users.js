f1267abaecce0a0092ce540d77a33ad30c606f3e
CylonVR <thefirstmellon@gmail.com>
2024-11-20 20:33:53 -0500

    Update update-users.js

--- update-users.js
+++ update-users.js
@@ -1,42 +1,35 @@
-// Created by Spokeek and edit by Ranily
-const EVENT_RAW = process.env.EVENT_RAW;
-const FILE_PATH = process.env.FILE_PATH || "./vrchat-kofi-users.txt"
 
-const fs = require('fs')
+const EVENT_RAW = process.env.EVENT_RAW;
+// FILE_PATH is not used since we won't be writing to a file
+// const FILE_PATH = process.env.FILE_PATH || "./vrchat-kofi-users.txt"
+
+const users = []; // Simulating the user array in memory for this example
 
 try {
-    const event_json = JSON.parse(EVENT_RAW.split("\n").join(""))
-    console.log(event_json)
+    const event_json = JSON.parse(EVENT_RAW.split("\n").join(""));
+    console.log(event_json);
 
-    const users = fs.readFileSync(FILE_PATH, {encoding: "utf8"});
-    const userArray = users.toString().split("\n").filter(n => n.trim().length).map((line) => line.split(":").map((item) => item.trim()));
     const type = event_json.type;
     let amount = event_json.amount;
 
-    if(type == "Subscription") {
-        amount = event_json.amount + "€/mois"
-        console.log("Type Subscription")
+    if (type == "Subscription") {
+        amount = event_json.amount + "€/mois";
+        console.log("Type Subscription");
+    } else {
+        amount = event_json.amount + "€";
+        console.log("Type Donation");
     }
-    else {
-        amount = event_json.amount + "€"
-        console.log("Type Donation")
-    }
-    
-    if(!userArray.find(line => line[0] === event_json.name)) {
-        userArray.push([event_json.name, amount])
-        console.log("user added")
-    }
-    else {
-        const index = userArray.findIndex(line => line[0] === event_json.name)
-        userArray[index][1] = amount
-        console.log("updated existing user")
-    }
-    console.log(userArray)
 
-    const textRaw = userArray.map((line) => line.join(" : ")).join("\n")
-    fs.writeFileSync(FILE_PATH, textRaw, { encoding: "utf8" })
-    console.log("file written")
-}
-catch(err) {
-    console.error({err, EVENT_RAW})
+    // Only register the user if they are not already in the user array
+    if (!users.find(line => line[0] === event_json.name)) {
+        users.push([event_json.name, amount]);
+        console.log("user added");
+        output = { userAdded: true }; // Indicate that a user was added
+    } else {
+        console.log("user already exists, not adding");
+        output = { userAdded: false }; // Indicate that no user was added
+    }
+} catch (err) {
+    console.error({ err, EVENT_RAW });
+    output = { error: err.message }; // Capture any errors
 }
